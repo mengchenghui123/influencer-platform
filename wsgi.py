e67272a41597bfb025f4d949ab6c894d1aad03b7
mengchenghui123 <itdep@mengchenghui.com>
2024-11-13 16:51:55 -0500

    requirement upadate

--- wsgi.py
+++
@@ -1,6 +0,0 @@
-# wsgi.py
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'influencer_platform.settings')
-app = get_wsgi_application()
