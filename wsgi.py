ee1148c835c67e82a992240ba0bc2f0740c13382
mengchenghui123 <itdep@mengchenghui.com>
2024-11-13 11:57:41 -0500

    requirement add

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'influencer_platform.settings')
 app = get_wsgi_application()
