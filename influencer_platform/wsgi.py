38701dac070090e8bc32ee285a3d542f325ac609
mengchenghui123 <itdep@mengchenghui.com>
2024-11-14 14:43:14 -0500

    wsgi.py app update

--- influencer_platform/wsgi.py
+++ influencer_platform/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'influencer_platform.settings')
 
-application = get_wsgi_application()
+app = get_wsgi_application()  # 将 application 重命名为 app
+
