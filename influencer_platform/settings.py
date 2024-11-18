4044eafeb8f4f0891afa4363cd4c8fdaf619d7e6
mengchenghui123 <itdep@mengchenghui.com>
2024-11-17 22:21:24 -0500

    static root update

--- influencer_platform/settings.py
+++ influencer_platform/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if DEBUG:
